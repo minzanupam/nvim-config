21ec68d3404c59909ae97770d8900ac443c42dbe
Anupam Ashish Minz <anupamminz300am@gmail.com>
2023-09-09 14:47:54 +0530

    modify to myliking and added snippets

---
+++ lua/custom/configs.lua
@@ -0,0 +1,31 @@
+vim.api.nvim_create_autocmd('BufReadPost', {
+  callback = function()
+    require('rust-tools').inlay_hints.enable()
+  end,
+  group = nil,
+  pattern = '*.rs',
+})
+
+vim.keymap.set("n", "<leader>ha", require("harpoon.mark").add_file)
+vim.keymap.set("n", "<leader>hl", require("harpoon.ui").toggle_quick_menu)
+
+vim.keymap.set("n", "<C-h>", function() require("harpoon.ui").nav_file(1) end)
+vim.keymap.set("n", "<C-t>", function() require("harpoon.ui").nav_file(2) end)
+vim.keymap.set("n", "<C-l>", function() require("harpoon.ui").nav_file(3) end)
+vim.keymap.set("n", "<leader>ht", function() require("harpoon.term").gotoTerminal(1) end)
+
+local group_code_format = vim.api.nvim_create_augroup('group_code_format', {})
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = { "*.go", "*.rs" },
+  group = group_code_format,
+  command = ":Neoformat",
+})
+
+vim.keymap.set("n", "<C-j>", function() vim.cmd("cnext") end)
+vim.keymap.set("n", "<C-k>", function() vim.cmd("cprev") end)
+
+vim.g.neovide_scroll_animation_length = 0
+vim.g.neovide_cursor_animation_length = 0
+vim.g.neovide_cursor_trail_size = 0
+vim.o.guifont = "Fira Code:h12" 
+
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,85 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+return {
+	-- auto closing parenthesise, brackets, etc
+	{ 'windwp/nvim-autopairs', opts = {} },
+
+	-- lsp extestions
+	{ 'hrsh7th/cmp-buffer' },
+	{ 'hrsh7th/cmp-path' },
+
+	-- for colorscheme
+	{
+		'ThemerCorp/themer.lua',
+		opts = {
+			colorscheme = "gruvbox",
+			transparent = true,
+			term_colors = true,
+
+			styles = {
+				constant = { style = "bold" },
+				constantBuiltIn = { style = "bold" },
+				keyword = { style = "italic" },
+				keywordBuiltIn = { style = "italic" },
+			},
+			plugins = {
+				treesitter = true,
+				indentline = true,
+				barbar = true,
+				bufferline = true,
+				cmp = true,
+				gitsigns = true,
+				lsp = true,
+				telescope = true,
+			},
+			remaps = {
+				palette = {
+					gruvbox = {
+						["dimmed"] = { ["subtle"] = "#7c6f64" },
+						["bg"] = { ["base"] = "#1d2021" },
+					},
+					tokyodark = {
+						["bg"] = { ["selected"] = "#32334a" },
+					},
+				},
+				highlights = {
+					gruvbox = {
+						base = {
+							ColorColumn = { bg = "#3c3836" },
+						},
+					},
+					nord = {
+						base = {
+							ColorColumn = { bg = "#3b4252" }
+						},
+					},
+					tokyodark = {
+						base = {
+							ColorColumn = { bg = "#32334a" }
+						},
+					},
+					tokyonight = {
+						base = {
+							ColorColumn = { bg = "#32334a" }
+						},
+					},
+				},
+			},
+		}
+	},
+
+	-- undo tree
+	{ 'mbbill/undotree' },
+
+	-- treesitter playground to view the treesitter AST
+	{ "nvim-treesitter/playground" },
+
+	-- improve rust integration nvim
+	{
+		'simrat39/rust-tools.nvim',
+		opts = {}
+	},
+
+	{ 'ThePrimeagen/harpoon', opts = {} },
+
+	-- to format code
+	{ 'sbdchd/neoformat' },
+}
