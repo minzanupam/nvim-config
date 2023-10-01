eee96cee46c5992de246f127ce3ed2decac133d7
Anupam Ashish Minz <anupamminz300am@gmail.com>
2023-10-01 14:42:07 +0530

    new plugins added

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -82,4 +82,14 @@ return {
 
 	-- to format code
 	{ 'sbdchd/neoformat' },
+
+    {'mattn/emmet-vim'},
+    {
+      "ray-x/lsp_signature.nvim",
+      event = "VeryLazy",
+      opts = {},
+      config = function(_, opts) require'lsp_signature'.setup(opts) end
+    },
+    {'dhruvasagar/vim-table-mode'},
+
 }
