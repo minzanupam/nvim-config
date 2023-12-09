c503cc36db8d7f89aef2319cd2b5e657fb6cc2e0
Anupam Ashish Minz <anupamminz300am@gmail.com>
2023-12-09 11:33:01 +0530

    back to autoformat

--- init.lua
+++ init.lua
@@ -117,7 +117,7 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
 
-  -- require 'kickstart.plugins.autoformat',
+  require 'kickstart.plugins.autoformat',
   -- require 'kickstart.plugins.debug',
   { import = 'custom.plugins' },
 }, {})
