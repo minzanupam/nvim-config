7c0d6a97d1371c2c00f1b49357af33087c5e045d
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-07-24 12:57:45 +0530

    function to print current date

--- lua/custom/functions.lua
+++ lua/custom/functions.lua
@@ -1,3 +1,16 @@
-function print_hello()
-  vim.api.nvim_put({ "hello world" }, "", false, true)
+local function run_command(cmd)
+  local f = io.popen(cmd, 'r')
+  if f then
+    local output = f:read("*l")
+    f:close()
+    return output
+  else
+    print("Error running command", cmd)
+    return nil
+  end
+end
+
+function print_date()
+  local today = run_command("date +%Y-%m-%d")
+  vim.api.nvim_put({ today .. " " }, "", false, true)
 end
