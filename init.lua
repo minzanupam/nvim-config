dd5e5c639e25f85baa0fcac98fa2a0d1991c54ff
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-08-23 12:21:05 +0530

    remove lualine

--- init.lua
+++ init.lua
@@ -80,14 +80,14 @@ require('lazy').setup({
 
   {
     'nvim-lualine/lualine.nvim',
-    opts = {
-      options = {
-        icons_enabled = false,
-        theme = 'onedark',
-        component_separators = '|',
-        section_separators = '',
-      },
-    },
+    -- opts = {
+    --   options = {
+    --     icons_enabled = false,
+    --     theme = 'onedark',
+    --     component_separators = '|',
+    --     section_separators = '',
+    --   },
+    -- },
   },
 
   {
