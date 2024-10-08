56bbfb4a4f51b54a9772a8ceea0697a5c45ab76f
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-10-08 11:02:08 +0530

    find hidden files

--- init.lua
+++ init.lua
@@ -213,7 +213,7 @@ local builtin = require('telescope.builtin')
 -- vim.keymap.set('n', '<leader>pf', function()
 --   require('telescope.builtin').git_files { show_untracked = true }
 -- end, { desc = 'Search [P]roject Git [F]iles' })
-vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = '[P]roject search [F]iles' })
+vim.keymap.set('n', '<leader>pf', function() builtin.find_files({hidden=true}) end, { desc = '[P]roject search [F]iles' })
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
