873d8d4276f8ea08a20c3bb1fb09f70d9e9427e1
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-11-08 01:07:39 +0530

    change find files back to hidden

--- init.lua
+++ init.lua
@@ -213,7 +213,7 @@ local builtin = require('telescope.builtin')
 -- vim.keymap.set('n', '<leader>pf', function()
 --   require('telescope.builtin').git_files { show_untracked = true }
 -- end, { desc = 'Search [P]roject Git [F]iles' })
-vim.keymap.set('n', '<leader>pf', function() builtin.find_files({ hidden = true }) end,
+vim.keymap.set('n', '<leader>pf', function() builtin.find_files({ hidden = false }) end,
   { desc = '[P]roject search [F]iles' })
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
