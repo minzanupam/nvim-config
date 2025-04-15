49d2f739d3a61975ec593d0aabf1b00d4c89f980
anupam <anupamminz300am@gmail.com>
2025-04-15 16:48:56 +0530

    add shortcut to reset hunk

--- init.lua
+++ init.lua
@@ -58,6 +58,7 @@ require('lazy').setup({
         vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
         vim.keymap.set('n', '<leader>hs', require('gitsigns').stage_hunk, { buffer = bufnr, desc = 'stage hunk' })
         vim.keymap.set('n', '<leader>hu', require('gitsigns').undo_stage_hunk, { buffer = bufnr, desc = 'stage hunk' })
+        vim.keymap.set('n', '<leader>hr', require('gitsigns').reset_hunk, { buffer = bufnr, desc = 'stage hunk' })
 
         local gs = package.loaded.gitsigns
         vim.keymap.set({ 'n', 'v' }, ']c', function()
