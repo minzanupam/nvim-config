fa38ba5e612c3cc1543811965171f7225738f537
anupam <anupamminz300am@gmail.com>
2025-07-16 11:09:41 +0530

    disable diagnostics

--- init.lua
+++ init.lua
@@ -526,6 +526,7 @@ require('lazy').setup({
           end,
         },
       }
+      vim.diagnostic.enable(false)
 
       -- LSP servers and clients are able to communicate to each other what features they support.
       --  By default, Neovim doesn't support everything that is in the LSP specification.
